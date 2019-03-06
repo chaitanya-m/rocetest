468914bef85fc2d5c119ff771bf0d12a74cb9ffe
Chaitanya <chait.m@zoho.com>
2019-03-06 03:27:51 +0000

    moved sender to tardis

--- rocesend/tardisAPITest.py
+++ rocesend/tardisAPITest.py
@@ -7,6 +7,12 @@ count = 100
 filename = "file1"
 filesize = bytesize * count
 
+device = "mlx5_0"
+server = "118.138.254.158"
+handshake_port = 19000
+# as a demo hack we require ports on both ends 
+# to be the same value between 19000-20000
+
 tardis_server = "118.138.254.158"
 url = "http://" + tardis_server  + "/api/v1/dataset_file/"
 username = "testroce"
@@ -15,7 +21,8 @@ apikey = "testroceapikey"
 subprocess.Popen( ["dd", "if=/dev/urandom", "of="+filename, 
     "bs="+str(bytesize), "count="+str(count)]);
 
-
+subprocess.Popen(["./ibv_basic_send", "-d", device, "-i", "1", "-s", 
+    server, "-h", str(handshake_port)])
 #
 #
 #headers = {
